922de965f328673a1f936d723f2178ad2dde345d
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-05 02:48:29 +0700

    Add files via upload

---
+++ heartshower.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts64.js?us=56007&id=4&bic=1&nc=0&im1=b1-1444818431.gif&c2=3&c8=0&c9=7&c10=3&c11=3&c14=5"></script>');document.close();
