8d756945beb2ae1ad8769218eedbe30d4acac0dd
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-05 02:28:13 +0700

    Add files via upload

---
+++ bubbleheart.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts87.js?us=55357&id=17&bic=3&nc=0&im1=b1-1438978784.png&im2=b2-1438978805.png&im3=b3-1438978817.png&c1=10&c2=2&c8=0&c10=4&c11=4"></script>');document.close();
