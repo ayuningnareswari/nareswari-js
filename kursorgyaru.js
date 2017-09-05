54af6ac42bb5f9bb36d44294309cb84023e52d5d
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-06 03:09:24 +0700

    Add files via upload

---
+++ kursorgyaru.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts4001.js?us=51672&id=33&bic=1&nc=0&im1=b1-1504635052.gif&c2=3"></script>');document.close();
