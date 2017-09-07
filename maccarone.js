3bfd6c9a9d0a23dbcfdb4d53989ae530ff2ad66d
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-08 03:23:25 +0700

    Create maccarone.js

---
+++ maccarone.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts13.js?us=40596&id=126&bic=1&nc=0&im1=b1-1407259377.gif&c1=20&c2=2&c3=0&c4=100"></script>');document.close();
