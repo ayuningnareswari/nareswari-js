3439c86c61879521be8aa577f12406a34555373a
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-08 03:12:18 +0700

    Create comebackhome1.js

---
+++ comebackhome1.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts135.js?us=51672&id=26&bic=1&nc=0&im1=b1-1412150101.jpg&c18=6&c26=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DkcTVTQ2UWg4&c28=0"></script>');document.close();
