2c559ebd5436e88d8d6964073b9fc6ee71efc082
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-08 03:34:36 +0700

    Create flowerfirework.js

---
+++ flowerfirework.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts63.js?us=48211&id=39&bic=7&nc=0&im1=b1-1409736608.gif&im2=b2-1409736616.gif&im3=b3-1409736620.gif&im4=b4-1409736624.gif&im5=b5-1409736631.gif&im6=b6-1409736667.gif&im7=b7-1409736670.gif&c2=3&c3=0&c4=150&c8=1&c9=7&c10=3&c11=3&c14=10"></script>');document.close();
