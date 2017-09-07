10d1ad4e4247a4df71977364a4937e27377fa09e
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-08 03:40:44 +0700

    Create tonefall.js

---
+++ tonefall.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts13.js?us=51672&id=10&bic=1&nc=0&im1=b1-1411050565.gif&c1=20&c2=3&c3=0&c4=100"></script>');document.close();
