29940b6b2759120fd170617bab90c9fc843541f2
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-08 05:44:21 +0700

    Create bluedandelion.js

---
+++ bluedandelion.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts86.js?us=51672&id=36&bic=1&nc=0&im1=b1-1504810901.png&c1=20&c2=3"></script>');document.close();
