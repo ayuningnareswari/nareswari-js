b35991984cc67ca4fbef58fed51eed983da4aea4
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-08 03:38:41 +0700

    Create girlballoon.js

---
+++ girlballoon.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts2001.js?us=51672&id=12&bic=1&nc=0&im1=b1-1411052461.gif&c1=10&c2=3&c3=0"></script>');document.close();
