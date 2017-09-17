dd36c94d82ccdb84c8800dac8de2bad2163d734e
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-17 15:26:36 +0700

    Update ashlovefirework.js

--- ashlovefirework.js
+++ ashlovefirework.js
@@ -1,1 +1,1 @@
-document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts65.js?us=51672&id=38&bic=4&nc=0&im1=b1-1505629209.gif&im2=b2-1505629220.gif&im3=b3-1505629235.gif&im4=b4-1505629269.gif&c2=3&c3=4&c8=1&c9=7&c10=4&c11=4"></script>');document.close();
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts65.js?us=51672&id=38&bic=4&nc=0&im1=b1-1505629209.gif&im2=b2-1505629220.gif&im3=b3-1505629235.gif&im4=b4-1505629269.gif&c2=3&c3=4&c8=0&c9=7&c10=4&c11=3"></script>');document.close();
