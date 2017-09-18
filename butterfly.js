3d6947f0b75549adb20cf88c8da413730a7f6c43
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-18 18:32:46 +0700

    Create butterfly.js

---
+++ butterfly.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts32.js?us=51672&id=39&bic=2&nc=0&im1=b1-1505716835.gif&im2=b2-1505727195.gif&c1=10&c2=1&c3=3&c4=50&c5=1"></script>');document.close();
