00d86fb0c85cf95b2b0f8be75e126b02a6335a97
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2017-09-19 14:22:41 +0700

    Create diamonddust.js

---
+++ diamonddust.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts13.js?us=39929&id=83&bic=7&nc=0&im1=b9-1382562938.gif&im2=b10-1382563227.gif&im3=b2-1382563216.gif&im4=b3-1382563216.gif&im5=b4-1382563216.gif&im6=b5-1382563216.gif&im7=b6-1382563216.gif&c1=30&c2=1&c3=3&c4=100"></script>');document.close();
