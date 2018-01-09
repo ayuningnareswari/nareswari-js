54ef93bc718303029061ce4a52da81c88b038fa0
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-09 12:13:33 +0700

    Create kursorheartsparkle.js

---
+++ kursorheartsparkle.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts44.js?us=51672&id=43&bic=3&nc=0&im1=b1-1515419691.gif&im2=b2-1515419708.gif&im3=b3-1515419747.gif&c6=3&c7=3&c8=0&c12=1&c13=2"></script>');document.close();
