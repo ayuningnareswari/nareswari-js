b15d28506df4d60856355560663559bb03d43b38
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-11 17:42:30 +0700

    Create shoujo.js

---
+++ shoujo.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts4001.js?us=34807&id=61&bic=1&nc=0&im1=b1-1491101135.png&c2=3"></script>');document.close();
