bf001279f3f19a1fa637b7ab662bb966d1311a44
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-15 10:04:09 +0700

    Create fireworkcolorful.js

---
+++ fireworkcolorful.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts62.js?us=51672&id=44&bic=5&nc=0&im1=b1-1515661298.gif&im2=b2-1515661312.gif&im3=b3-1515661379.gif&im4=b4-1515661420.gif&im5=b5-1515662665.gif&c8=1&c10=3&c11=4"></script>');document.close();
