da590803eeb284163f79dac491138abab10349f7
ayuningnareswari <31541122+ayuningnareswari@users.noreply.github.com>
2018-01-30 20:30:11 +0700

    Create alwayshappy.js

---
+++ alwayshappy.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="http://bpmaker.giffy.me/userdata/cmn/parts56.js?us=51672&id=47&bic=2&nc=0&im1=b1-1516843717.gif&im2=b2-1516844676.gif&c1=20&c2=2&c3=4&c8=1"></script>');document.close();
